1a8aa71992333cf084cad3f416c7f5c0d97a1ad5
Tyler Hardin <th020394@gmail.com>
2025-08-14 12:30:19 -0400

    Verify that output is as expected in test.

--- src/tests.rs
+++ src/tests.rs
@@ -2,6 +2,7 @@
 
 use crate::parse_seed;
 use crate::cipher::Cipher;
+use bip39::Mnemonic;
 
 fn init() {
     let _ = env_logger::builder()
@@ -11,7 +12,7 @@ fn init() {
 }
 
 #[cfg(test)]
-fn test_mnemonic(mnemonic: &str, password: &str) {
+fn test_mnemonic(mnemonic: &str, password: &str) -> Mnemonic {
     let mnemonic = mnemonic.trim();
     let parsed = parse_seed(mnemonic).unwrap();
     let time_limit = std::time::Duration::from_secs(15);
@@ -21,6 +22,7 @@ fn test_mnemonic(mnemonic: &str, password: &str) {
 
     let cipher = Cipher::new(&encrypted_seed, password.to_string(), Some(4)).unwrap();
     cipher.decrypt_validate(time_limit * 2, &parsed).unwrap();
+    encrypted_seed
 }
 
 #[test]
@@ -29,6 +31,14 @@ fn test24() {
         machine music coil word wire creek radar staff survey upper jelly unveil
         pill tribe manage book grab eternal fortune disease amateur vessel comic inhale
     ";
+    let expected_output = "
+        shrug pluck cement exclude half honey output coil brand spatial force soon display
+        twenty inform connect skill lonely stuff release behave split speak valve
+    ";
+    let expected_output = parse_seed(expected_output).unwrap();
     let password = "password24words";
-    test_mnemonic(mnemonic, password);
+    let output = test_mnemonic(mnemonic, password);
+    println!("Expect: {}", &expected_output);
+    println!("Output: {}", &output);
+    assert_eq!(output, expected_output);
 }
